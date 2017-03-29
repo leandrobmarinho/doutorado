7cecde29c61d7d3c4e936e902a5e9a8cb952e0c2
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2017-03-29 00:43:50 -0300

    pfft

--- transformadas/ifft_from_fft/ifft_from_fft.m
+++ transformadas/ifft_from_fft/ifft_from_fft.m
@@ -7,7 +7,7 @@ subplot(2,3,1), imshow(img);
 title('Imagem original.')
 
 % Transformada
-transformada = fft2(img);
+transformada = (fft2(img));
 
 % Plota a transformada
 subplot(2,3,2), imshow(mat2gray(log(abs(transformada)))) 
@@ -22,12 +22,53 @@ infft1 = ifft2(transformada);
 infft2 = conj(fft2(conj(transformada)))/n;
 infft3 = fft2(conj(transformada))/n;
 
-subplot(2,3,4), imshow(uint8(abs(infft1)));
+subplot(2,3,4), imshow(uint8(infft1));
 title('Inversa com ifft2.')
-subplot(2,3,5), imshow(uint8(abs(infft2)));
+subplot(2,3,5), imshow(uint8(infft2));
 title('Inversa com (fft(x*)*)/n.')
-subplot(2,3,6), imshow(uint8(abs(infft3)));
+subplot(2,3,6), imshow(uint8(infft3));
 title('Inversa com fft(x*)/n.')
+set(gcf,'units','normalized','outerposition',[0 0 1 1])
+
+infft1 = ifft2(transformada);
+infft2 = conj(fft2(conj(transformada)))/n;
+infft3 = fft2(conj(transformada))/n;
+
+figure
+subplot(2,3,1), imshow(uint8(infft1));
+title('Inversa com ifft2.')
+subplot(2,3,2), imshow(uint8(infft2));
+title('Inversa com (fft(x*)*)/n.')
+subplot(2,3,3), imshow(uint8(infft3));
+title('Inversa com fft(x*)/n.')
+
+transformada = fftshift(transformada);
+infft1 = ifft2(transformada);
+infft2 = conj(fft2(conj(transformada)))/n;
+infft3 = fft2(conj(transformada))/n;
+
+subplot(2,3,4), imshow(uint8(infft1));
+title('Inversa com ifft2 (shift na transformada).')
+subplot(2,3,5), imshow(uint8(infft2));
+title('Inversa com (fft(x*)*)/n (shift na transformada).')
+subplot(2,3,6), imshow(uint8(infft3));
+title('Inversa com fft(x*)/n (shift na transformada).')
+set(gcf,'units','normalized','outerposition',[0 0 1 1])
+
+
+figure
+transformada = fftshift(transformada);
+% subplot(2,3,1), imshow(angle(transformada))
+% title('Fase')
+
+
+subplot(1,2,1), imshow(img)
+title('Imagem original.')
+subplot(2,2,2), imshow(mat2gray(log(abs(fftshift(transformada)))))
+title('Magnitude da Transformada')
+subplot(2,2,4), imshow(angle(transformada))
+title('Fase da Transformada')
+set(gcf,'units','normalized','outerposition',[0 0 1 1])
 
 
 %% Apenas uma linha da imagem
@@ -43,3 +84,14 @@ infft3 = fft(conj(trans_sin))/n;
 mse(sinal, real(infft1))
 mse(sinal, real(infft2))
 mse(sinal, real(infft3))
+
+figure
+subplot(2,2,1), plot(sinal)
+title('Sinal original.')
+subplot(2,2,2), plot(abs(trans_sin))
+title('Magnitude do sinal.')
+subplot(2,2,3), plot(real(infft2))
+title('Inversa com (fft(x*)*)/n.')
+subplot(2,2,4), plot(real(infft3))
+title('Inversa com fft(x*)/n.')
+set(gcf,'units','normalized','outerposition',[0 0 1 1])
--- transformadas/runFT.m
+++ transformadas/runFT.m
@@ -1,7 +1,7 @@
 close all; clear; clc;
 img = imread('../data/imgs/lenna.jpg');
 
-img=zeros(30,30); img(5:24,13:17)=1;
+% img=zeros(30,30); img(5:24,13:17)=1;
 
 
 F1 = fft2(img);
@@ -14,6 +14,6 @@ F2_abs = abs(F2);
 
 figure, imshow(uint8(ifft2(fftshift((F1)))))
 figure, imshow(uint8(ifft2(((F1)))))
-figure, imshow(fftshift(F1_abs),[], 'InitialMagnification','fit')
+% figure, imshow(fftshift(F1_abs),[], 'InitialMagnification','fit')
 % figure, imshow(fftshift(F2_abs),[], 'InitialMagnification','fit')
 % figure, surf(ang1)
